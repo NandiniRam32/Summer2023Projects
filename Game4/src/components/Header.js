48526caa89651344eaad58932f10f062c53952fb
Nandini Ramakrishnan <126700503+NandiniRam32@users.noreply.github.com>
2023-07-08 16:31:20 -0700

    Add Header file

---
+++ Game4/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "tailwindcss/tailwind.css";
+
+const Header = () => {
+    return (
+        <div style={{width: "30vw"}} className="header">
+            <big className="header-title">
+                Number<br />Islands
+            </big>
+            <button className="dot" style={{fontFamily: "papyrus, fantasy"}}>
+                Light/Dark Mode
+            </button>
+        </div>
+    );
+};
+
+export default Header;
