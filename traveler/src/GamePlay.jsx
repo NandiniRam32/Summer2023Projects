3b2c2dbe2a25feb37c76eac88a2c8031300b62c6
rlhon <107014628+rlhon@users.noreply.github.com>
2023-07-09 22:50:38 -0700

    New Files For layouts and button functionality

---
+++ traveler/src/GamePlay.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "./index.css";
+
+export const GamePlay = ({ handleChangePage }) => {
+    
+    return (
+        <>
+            
+        </>
+    )
+}
